13395a9f6d6ebfdc7beb8df60601266a6782b344
lagb1703 <120032395+lagb1703@users.noreply.github.com>
2025-11-18 06:07:42 +0000

    agregado /

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,38 @@
-import { SearchChats } from "@/components/search-chats"
-import { ChatGrid } from "@/components/chat-grid"
 import { CreateChatButton } from "@/components/create-chat-button"
 import { AppHeader } from "@/components/app-header"
 import { AppFooter } from "@/components/app-footer"
-import { Brain } from "lucide-react"
+import Image from "next/image"
+import { Badge } from "@/components/ui/badge"
+
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-background flex flex-col">
+    <div className="min-h-screen bg-[#ffffff] flex flex-col">
       <AppHeader showUserMenu={false} />
 
       <main className="flex-1">
         {/* Hero Section */}
-        <div className="bg-gradient-to-b from-primary/5 to-transparent py-16 px-6">
-          <div className="max-w-7xl mx-auto text-center mb-12">
-            <div className="inline-flex items-center justify-center w-20 h-20 rounded-full bg-primary/10 mb-6">
-              <Brain className="w-10 h-10 text-primary" />
+        <div className="max-w-7xl mx-auto text-center mb-1 pt-6">
+            <div className="inline-flex items-center justify-center w-[15vw] h-[15vw] relative">
+              <Image src="/tinyLogoBlue.png" alt="Logo" fill className="object-contain" />
             </div>
-            <h2 className="text-4xl md:text-5xl font-bold text-foreground mb-4">ChatHub</h2>
-            <p className="text-xl text-muted-foreground mb-2">
-              <span className="text-accent font-semibold">Inteligencia Artificial</span>
-            </p>
-            <p className="text-lg text-muted-foreground max-w-2xl mx-auto">
-              Accede a múltiples chats de IA potenciados por las últimas tecnologías de inteligencia artificial
+            <p className="text-[5vw] text-muted-foreground mb-2 font-black leading-[0.85]">
+              <span className="block text-[#016FD8]">GOOD</span>
+              <span className="block text-[#00D8B5] mt-1">HEALTH</span>
             </p>
           </div>
-        </div>
-
+          <div className="w-full flex justify-center">
+            <div className="w-fit flex justify-center p-4 space-x-2 bg-[#f6f6f6] rounded-full">
+              <Badge variant="secondary">Secondary</Badge>
+              <Badge variant="secondary">Secondary</Badge>
+              <Badge variant="secondary">Secondary</Badge>
+            </div>
+          </div>
+          <div className="max-w-7xl mx-auto text-center mb-1 pt-6">
+            <div className="inline-flex items-center justify-center w-[60vw] h-[15vw] relative">
+              <Image src="/image.png" alt="Logo" fill className="object-contain" />
+            </div>
+          </div>
         {/* Main Content */}
       </main>
 
